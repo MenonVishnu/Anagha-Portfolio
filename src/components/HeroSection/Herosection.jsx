a1e1c6d5a8d37ef624b32167c35d743e139c21c2
Vishnu Menon <menonvishnu26@gmail.com>
2025-04-24 16:50:53 +0530

    Added Portfolio Section and Navbar Section

--- src/components/HeroSection/Herosection.jsx
+++ src/components/HeroSection/Herosection.jsx
@@ -5,16 +5,7 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <>
-      <div className="portfolio">
-        <div className="heading">
-          PORTFOLIO
-          <p className="ele-1">Design</p>
-          <p className="ele-2">Product</p>
-          <p className="ele-3">Experience</p>
-          <p className="ele-4">UI</p>
-          <p className="ele-5">UX</p>
-        </div>
-      </div>
+      <h1>Anagha in Images</h1>
     </>
   );
 };
