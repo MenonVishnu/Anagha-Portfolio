7ef468068856370c0d4c597537ea1bba6a6cd2c5
Vishnu Menon <menonvishnu26@gmail.com>
2025-04-24 17:51:52 +0530

    Hero Section Addition

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import HeroSection from "./components/PortfolioSection/PortfolioSection.jsx";
+import PortfolioSection from "./components/PortfolioSection/PortfolioSection.jsx";
+import HeroSection from "./components/HeroSection/Herosection.jsx";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <PortfolioSection />
       <HeroSection />
     </>
   );
--- src/components/HeroSection/Herosection.jsx
+++ src/components/HeroSection/Herosection.jsx
@@ -2,11 +2,30 @@ import "@fontsource/koulen"; // Defaults to weight 400
 import "@fontsource/kosugi-maru"; // Defaults to weight 400
 import "./HeroSection.css";
 
+// Image Import
+import AnaghaHero from "../../assets/Anagha-Image.png";
+import Name from "../../assets/Name.png";
+
 const HeroSection = () => {
   return (
-    <>
-      <h1>Anagha in Images</h1>
-    </>
+    <div className="hero-container">
+      <div className="circle"></div>
+      <div className="paragraph">
+        <img src={AnaghaHero} alt="Anagha Image" />
+        <div className="hero-heading">Hello,</div>
+        <p>
+          I am <img src={Name} alt="anagha name" />{" "}
+        </p>
+
+        <p>
+          Computer science graduate with hands-on experience in UI/UX design,
+          web development, & AI/ML.I thrive on solving problems & sparking
+          innovation! Passionate about user interaction & experience design,I
+          love diving into case studies for industry-leading products and
+          crafting engaging wire-frames.{" "}
+        </p>
+      </div>
+    </div>
   );
 };
 
