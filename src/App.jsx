44eee8b67774581bd6bc40b06f5c9e3f9e84bc06
Vishnu Menon <menonvishnu26@gmail.com>
2025-07-14 21:38:18 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import PortfolioSection from "./components/PortfolioSection/PortfolioSection.jsx";
-import HeroSection from "./components/HeroSection/Herosection.jsx";
+import HeroSection from "./components/HeroSection/HeroSection.jsx";
 import SkillAndTools from "./components/SkillAndTools/SkillAndTools.jsx";
 import ProjectSection from "./components/ProjectSection/ProjectSection.jsx";
 import CaseStudy from "./components/CaseStudy/CaseStudy.jsx";
@@ -13,28 +13,28 @@ import ThankYou from "./components/ThankYou/ThankYou.jsx";
 import ProjectPage from "./components/ProjectSection/ProjectPage/ProjectPage.jsx";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <PortfolioSection />
-              <HeroSection />
-              <SkillAndTools />
-              <ProjectSection />
-              <CaseStudy />
-              <ThankYou />
-            </>
-          }
-        />
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<PortfolioSection />
+							<HeroSection />
+							<SkillAndTools />
+							<ProjectSection />
+							<CaseStudy />
+							<ThankYou />
+						</>
+					}
+				/>
 
-        <Route path="/projects/:id" element={<ProjectPage />} />
-      </Routes>
-    </Router>
-  );
+				<Route path="/projects/:id" element={<ProjectPage />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
